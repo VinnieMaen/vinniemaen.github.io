fa293290508692ef73d1f706f9af4d5cde292f8f
VinnieMaen <52604982+VinnieMaen@users.noreply.github.com>
2022-05-23 16:36:38 +0200

    Update animation.js

--- src/animation.js
+++ src/animation.js
@@ -176,7 +176,7 @@ document.getElementById("terminal").addEventListener("click", () => {
         cmdContainer.className = "cmdContainer"
         if (cl == "cmdContainerClosed") {
             startWrite(false, "Type help to get started!");
-
+            prompt()
             setTimeout(() => {
                 startWrite(true, "")
             }, 1000);
